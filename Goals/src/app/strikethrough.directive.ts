7711d81cdf81e420c9a64eb10ef096bc26c223a1
tomito26 <tommybwah@gmail.com>
2020-08-20 01:50:03 +0300

    created a new angular app project

---
+++ Goals/src/app/strikethrough.directive.ts
@@ -0,0 +1,17 @@
+import { Directive,ElementRef,HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+  constructor(private elem:ElementRef) { }
+  @HostListener('click') onclicks(){
+    this.textDeco('line-through')
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration = action;
+  }
+   
+
+}
